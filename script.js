9ea11c5f64deab2d3bd7f37f1439f4fbe6f4258b
yuhsin-Chen <jb4778527@gmail.com>
2025-05-22 15:38:19 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    const section = document.querySelector(this.getAttribute('href'));
-    section.scrollIntoView({ behavior: 'smooth' });
-  });
-});
