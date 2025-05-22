bb6e76c43111094bc338db9b3814b44effdb99de
yuhsin-Chen <jb4778527@gmail.com>
2025-05-22 15:39:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const section = document.querySelector(this.getAttribute('href'));
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+});
